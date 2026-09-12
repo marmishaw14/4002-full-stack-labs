4739856d5463a4ed07e51327668718069c1054eb
McCauley <marmishaw@academic.rrc.ca>
2026-09-11 23:09:45 -0500

    data: create interface for employee info and add in list of employees and departments

---
+++ src/models/employeeModel.ts
@@ -0,0 +1,8 @@
+/**
+ * Represents employee info at Pixell River Financial
+ */
+export interface EmployeeInfo {
+    employeeFirstName: string;
+    employeeLastName: string;
+    department: string;
+}
