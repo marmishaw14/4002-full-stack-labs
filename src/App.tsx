6d1219127e887da6e506c6796540fe60b196fa5a
McCauley <marmishaw@academic.rrc.ca>
2026-09-11 22:52:11 -0500

    refactor(App): update App file with Header and Footer components imported

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Footer from "./components/common/footer/Footer";
+import Header from "./components/common/header/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
