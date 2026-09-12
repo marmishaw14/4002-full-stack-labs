e365ed4eea85d26fdd04999e26e949860f498a01
McCauley <marmishaw@academic.rrc.ca>
2026-09-11 23:33:42 -0500

    data: update employee data and interface to include id for proper usage of React Lists

--- src/models/employeeModel.ts
+++ src/models/employeeModel.ts
@@ -2,6 +2,7 @@
  * Represents employee info at Pixell River Financial
  */
 export interface EmployeeInfo {
+    id: number;
     employeeFirstName: string;
     employeeLastName: string;
     department: string;
